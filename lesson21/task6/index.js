276fa33c37c8231eb395de590816d881b394bb3c
mamuryusupov <mark.yusupovv@gmail.com>
2021-12-13 16:21:16 +0200

    «new»

--- lesson21/task6/index.js
+++ lesson21/task6/index.js
@@ -1,8 +1,12 @@
-export const setButton = function (buttonText) {
-  const newBut = document.createElement('button');
-  newBut.textContent = buttonText;
-  const butSet = (document.querySelector('body').innerHTML = newBut);
+// const setButton = function (buttonText) {
+//   const newBut = document.createElement('button');
+//   newBut.textContent = buttonText;
+//   const butSet = (document.querySelector('body').innerHTML = 'newBut');
+//   return butSet;
+// };
+
+setButton('Hello');
+const setButton = function (buttonText) {
+  const butSet = (document.querySelector('body').innerHTML = '<button>buttonText</button>');
   return butSet;
 };
-
-setButton();
