e7b1267065d218703281fb561d349d9b56b25ee4
mamuryusupov <mark.yusupovv@gmail.com>
2021-12-13 17:11:53 +0200

    «new»

---
+++ lesson21/task8/index.js
@@ -0,0 +1,6 @@
+export const setButton = function (buttonText) {
+  const newBut = document.createElement('button');
+  newBut.textContent = buttonText;
+  const butSet = document.querySelector('body').append(newBut);
+  return butSet;
+};
