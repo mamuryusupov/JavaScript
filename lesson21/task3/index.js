b3e4807a211b6b08b2fba919b609aaa20ccc1e22
mamuryusupov <mark.yusupovv@gmail.com>
2021-12-13 14:34:57 +0200

    «new»

--- lesson21/task3/index.js
+++ lesson21/task3/index.js
@@ -1,12 +1,12 @@
-const getItemsList = function () {
+/* export*/ const getItemsList = function () {
   const elementsList = document.querySelectorAll('.technology');
   console.dir(elementsList);
   return elementsList;
 };
 
-const getItemsArray = function () {
-  const elementsArray = Array.from(document.querySelectorAll('.tool'));
-  console.dir(elementsArray);
+/* export*/ const getItemsArray = function () {
+  const elementsArray = document.querySelectorAll('.tool');
+  console.dir(Array.from(elementsArray));
   return elementsArray;
 };
 
