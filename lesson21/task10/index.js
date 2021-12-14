aa34fb07102837a925ce56db762b16c56905c9f4
mamuryusupov <mark.yusupovv@gmail.com>
2021-12-14 11:25:22 +0200

    «new»

--- lesson21/task10/index.js
+++ lesson21/task10/index.js
@@ -1,10 +1,11 @@
 export const finishForm = function () {
-  const formElem = document.querySelector('form');
+  const formElem = document.querySelector('.login-form');
+  const passwordForm = document.querySelector('input');
+
   const newAttribute = document.createElement('input');
-  newAttribute.setAttribute('type', text);
+  newAttribute.setAttribute('type', 'text');
   newAttribute.setAttribute('name', 'login');
   formElem.prepend(newAttribute);
 
-  const passwordForm = document.querySelector('input');
   passwordForm.setAttribute('type', password);
 };
