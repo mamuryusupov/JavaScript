b50b6741765bd5ad0fe67b7fcdbc09ce5cdb5c9e
mamuryusupov <mark.yusupovv@gmail.com>
2021-12-14 11:19:07 +0200

    «new»

--- lesson21/task11/index.js
+++ lesson21/task11/index.js
@@ -1,6 +1,9 @@
-export const createButton = function (buttonText) {
-  const newBut = document.createElement('button');
-  newBut.textContent = buttonText;
-  const butSet = document.querySelector('body').append(newBut);
-  return butSet;
+const manageClasses = function () {
+  document.querySelector('.class').classList.add('.selected');
+  document.querySelector('.two').classList.remove('.selected');
+  document.querySelector('.three').classList.toggle('.three_done');
+  const liElem = document.querySelector('.four').classList.contains('.some-class');
+  if (liElem) {
+    liElem.classList.add('.another-class');
+  }
 };
