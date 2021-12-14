ff83bc2f6e495b9dfaee436d805c1e893666e255
mamuryusupov <mark.yusupovv@gmail.com>
2021-12-14 14:32:17 +0200

    «new»

--- lesson21/task13/index.js
+++ lesson21/task13/index.js
@@ -1,6 +1,6 @@
 export { getSection };
 const getSection = num => {
-  const spanElem = document.querySelector(span[data-number ="${num}"]);
+  const spanElem = document.querySelector(`span[data-number ="${num}"]`);
   const closestParent = spanElem.closest('.box');
   return closestParent.dataset.section;
 };
