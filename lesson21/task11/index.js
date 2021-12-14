330c18ec171eeb33341382112c414bd4b33dc1ca
mamuryusupov <mark.yusupovv@gmail.com>
2021-12-14 11:35:55 +0200

    «new»

--- lesson21/task11/index.js
+++ lesson21/task11/index.js
@@ -1,15 +1,15 @@
-export const manageClasses = function () {
-  const firstElem = document.querySelector('.class');
-  firstElem.classList.add('.selected');
+export const manageClasses = () => {
+  const firstElem = document.querySelector('class');
+  firstElem.classList.add('selected');
 
-  const secondElem = document.querySelector('.two');
-  secondElem.classList.remove('.selected');
+  const secondElem = document.querySelector('two');
+  secondElem.classList.remove('selected');
 
   const thirdElem = document.querySelector('.three');
-  thirdElem.classList.toggle('.three_done');
+  thirdElem.classList.toggle('three_done');
 
   const fourthElem = document.querySelector('.four');
-  if (fourthElem.classList.contains('.some-class')) {
-    liElem.classList.add('.another-class');
+  if (fourthElem.classList.contains('some-class')) {
+    liElem.classList.add('another-class');
   }
 };
