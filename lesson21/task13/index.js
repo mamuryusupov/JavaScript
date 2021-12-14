ce6f9d0dbed5d21bdb3d33ff3b3b116f730c247a
mamuryusupov <mark.yusupovv@gmail.com>
2021-12-14 13:57:45 +0200

    «new»

--- lesson21/task13/index.js
+++ lesson21/task13/index.js
@@ -1,4 +1,8 @@
 export { getSection };
-const getSection = num => {};
+const getSection = num => {
+  const spanElem = document.querySelector(`span[data-number ='${num}']`);
+
+  return spanElem.parentElement.dataset.section;
+};
 
 getSection();
